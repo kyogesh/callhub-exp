ca15a2c4d3f044170335da694991b0dee494531d
Yogesh Kumar <yk.301189@gmail.com>
2018-09-18 08:04:02 +0530

    Update

--- callhub_exp/prod.py
+++ callhub_exp/prod.py
@@ -12,6 +12,18 @@ INSTALLED_APPS += ['api',
                    'rest_framework.authtoken',
                    ]
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'd6v722740qbkjl',
+        'USER': 'pitxnucxavybjg',
+        'PASSWORD': 'e8cef8c0e0e9ec29cf8c712c539f67287c32186fc366c130e2a4496f290985c1',
+        'HOST': 'ec2-174-129-32-37.compute-1.amazonaws.com',
+        'PORT': '5432',
+    }
+}
+
+
 # Use nose to run all tests
 TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
 
@@ -23,3 +35,4 @@ NOSE_ARGS = [
 
 
 django_heroku.settings(locals())
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
