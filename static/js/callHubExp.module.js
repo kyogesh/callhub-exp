58074cff7eda3de4e4bd9226db870a1fa5c51d61
Yogesh Kumar <yk.301189@gmail.com>
2018-09-18 08:59:10 +0530

    Update

--- static/js/callHubExp.module.js
+++ static/js/callHubExp.module.js
@@ -1,6 +1,6 @@
 (function () {
     "use strict";
     //ui.bootstrap is needed for Accordion
-    var callHubExp = angular.module('callHubExp', ['ngRoute', 'ui.bootstrap', 'ngCookies', 'ngTagsInput',
+    var callHubExp = angular.module('callHubExp', ['ngRoute', 'ngCookies', 'ngTagsInput',
         'ngAnimate', 'ngTouch', 'FBAngular', 'home', 'auth', 'tags', 'registration', 'ticket', 'fixedTop', 'footer']);
 }());
